3a0d5ef340d13f39aaba505626c600f93a914791
Stefan.Iliev <stefan.iliev94@gmail.com>
2016-12-05 16:16:42 +0200

    Update

---
+++ BlogSoftUni/Controllers/ArticleController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BlogSoftUni.Models;
+using System.Net;
+
+namespace BlogSoftUni.Controllers
+{
+    public class ArticleController : Controller
+    {
+        // GET: Article
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        //
+        // GET:Article/List
+        public ActionResult List()
+        {
+            using (var database = new BlogDbContext())
+            {
+                //Get articles from database
+                var articles = database.Articles
+                    .Include(a => a.Author)
+                    .ToList();
+                return View(articles);
+
+            }
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            using (var database = new BlogDbContext())
+            {
+                //Get the article from database
+                var article=database.Articles
+                .Where(a => a.Id == id)
+                   .Include(a => a.Author)
+                   .First();
+
+                if (article == null)
+                {
+                    return HttpNotFound();
+                }
+
+                return View(article);
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        //
+        //POST:Article/Create
+        [HttpPost]
+        public ActionResult Create(Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var database = new BlogDbContext())
+                {
+                    //Get author id
+                    var authorId = database.Users
+                        .Where(u => u.UserName == this.User.Identity.Name)
+                        .First().Id;
+                    //Set articles author
+                    article.AuthorId = authorId;
+                    //Save article in DB
+                    database.Articles.Add(article);
+                    database.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(article);
+        }
+    }
+}
---
+++ BlogSoftUni/Startup.cs
@@ -0,0 +1,20 @@
+﻿using BlogSoftUni.Models;
+using BlogSoftUni.Migrations;
+using Microsoft.Owin;
+using Owin;
+using System.Configuration;
+using System.Data.Entity;
+
+[assembly: OwinStartupAttribute(typeof(BlogSoftUni.Startup))]
+namespace BlogSoftUni
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            Database.SetInitializer(
+                new MigrateDatabaseToLatestVersion<BlogDbContext, Migrations.Configuration>());
+            ConfigureAuth(app);
+        }
+    }
+}
